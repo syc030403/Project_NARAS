0168f2ac6563935b093f22adfc2f8f59f15aa32f
syc030403 <syc030403@naver.com>
2025-09-02 20:35:52 +0900

    first committed

---
+++ src/pages/Country.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom"
+
+export default function Country(){
+
+    const params = useParams();
+    console.log(params);
+
+    return <div>Country : {params.code}</div>
+}
---
+++ src/pages/Search.jsx
@@ -0,0 +1,8 @@
+import { useSearchParams } from "react-router-dom"
+
+export default function Search(){
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    return <div>Search {searchParams.get("q")}</div>
+}
