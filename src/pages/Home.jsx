290b9f510d2f68f08824d8a21e974f076f651fe6
syc030403 <syc030403@naver.com>
2025-09-11 13:05:03 +0900

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { fetchCountries } from "../api";
+import { useEffect, useState } from "react";
+
 export default function Home(){
+
+    const [countries, setCountries] = useState();
+
+    // @ts-ignore
+    const setInitData = async() => {
+       const data = await fetchCountries();
+       setCountries(data);
+    }
+
+    useEffect(()=>{setInitData()}, [])
     return <div>Home</div>
 }
+
